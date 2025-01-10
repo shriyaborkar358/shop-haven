f4efb6a98dff94bad2a685cf87ef8842ed5971f4
unknown <shriyaborkar71@gmail.com>
2025-01-10 15:50:19 +0530

    Order model ready

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,40 +1,42 @@
 import { model, Schema } from "mongoose";
 
-const productSchema = new Schema ({
-    name:{
-        type: String,
-        required: true,
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    shortDescription:{
-        type: String,
-        required: true,
+    shortDescription: {
+      type: String,
+      required: true,
     },
-    longDescription:{
-        type: String,
-        required: true,
+    longDescription: {
+      type: String,
+      required: true,
     },
-    price:{
-        type: Number,
-        required: true,
+    price: {
+      type: Number,
+      required: true,
     },
-    currentPrice:{
-        type: Number,
+    currentPrice: {
+      type: Number,
     },
-    category:{
-        type: String,
-        required: true,
+    category: {
+      type: String,
+      required: true,
     },
-    images:{
-        type:[String],
-        required: true,
+    images: {
+      type: [String],
+      required: true,
     },
-    tags:{
-        type:[String]
-    }
-},
-{
+    tags: {
+      type: [String],
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const Product = model ("Product", productSchema);
-export default Product
+const Product = model("Product", productSchema);
+export default Product;
--- server/models/User.js
+++ server/models/User.js
@@ -1,34 +1,34 @@
 import { model, Schema } from "mongoose";
 
 const userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-        },
-        phone: {
-            type: String,
-            unique: true,
-        },
-        address: {
-            type: String,
-        },
-        role: {
-            type: String,
-            default: "user",
-        },
-        password: {
-            type: String,
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+    },
+    address: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = model("User", userSchema);
